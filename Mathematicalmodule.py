7edb4a69a92c7f5d5ac3c811d76677da014a075f
Vive1-Git <vivekprasad1905@gmail.com>
2024-11-08 00:07:53 +0530

    Create Mathematicalmodule.py

---
+++ Mathematicalmodule.py
@@ -0,0 +1,22 @@
+'''Creating module that contains functions for mathematical operations'''
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def modulus(a,b):
+    return a%b
+
+def square(a):
+    return a**2
+
+def square_root(a):
+    return a**(1/2)
